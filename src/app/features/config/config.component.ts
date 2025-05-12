4812343bd59a3a888f1a1148cbc959155e1676c7
ymandy1 <amanda.study110@gmail.com>
2025-05-11 22:29:44 -0300

    change layout organization and add front to config

--- src/app/features/config/config.component.ts
+++ src/app/features/config/config.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-config',
-  imports: [],
+  standalone: true, 
+  imports: [FormsModule],
   templateUrl: './config.component.html',
-  styleUrl: './config.component.css'
+  styleUrls: ['./config.component.css']
 })
 export class ConfigComponent {
+  user = { nome: '', contato: '', email: '' };
+  senhaAtual = '';
+  novaSenha = '';
+  repetirSenha = '';
 
+  onEdit() {
+  }
+
+  confirmDelete() {
+    Swal.fire({
+      title: 'Tem certeza?',
+      text: 'Esta ação irá excluir sua conta permanentemente!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#e74c3c',
+      cancelButtonColor: '#295c47',
+      confirmButtonText: 'Sim, deletar!',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.deleteAccount();
+      }
+    });
+  }
+
+  deleteAccount() {
+    Swal.fire('Deletado!', 'Sua conta foi excluída.', 'success');
+  }
 }
