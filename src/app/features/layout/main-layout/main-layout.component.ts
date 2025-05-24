4ef4b4df5ab77ce696840b3902788624ce3c19d4
ymandy1 <amanda.study110@gmail.com>
2025-05-24 17:23:25 -0300

    add resposive adjusts and fix layout

--- src/app/features/layout/main-layout/main-layout.component.ts
+++ src/app/features/layout/main-layout/main-layout.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
-export class MainLayoutComponent {
 
+export class MainLayoutComponent {
+   mostrarSidebar = false; 
+
+  fecharSidebar() {
+   this.mostrarSidebar = false;
+  }
 }
