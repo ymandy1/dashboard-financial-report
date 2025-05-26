6bc25bd7cc8dcaf6ff6299e1b59cddb768364732
miguellmoura <miguelmouracvm23@gmail.com>
2025-05-26 02:12:33 -0300

    adiciona lógica de login com backend

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,11 +1,41 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from './services/login.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  constructor(private loginService: LoginService, private router: Router) { }
+
+  email: string = '';
+  senha: string = '';
+
+  onSubmit() {
+    console.log('Email:', this.email);
+    console.log('Senha:', this.senha);
+
+    const body = {
+      "email": this.email,
+      "password": this.senha
+    }
+
+    this.loginService.login(body).subscribe({
+      next: (res: any) => {
+        this.router.navigate(['/dashboard'])
+      },
+      error: (err) => {
+        console.error('Erro no login:', err);
+      }
+    });
+  }
 }
+
+
+
